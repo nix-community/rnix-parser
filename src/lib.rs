5107d22c14239fffb32e0899b1e97a124549a4be
jD91mZM2 <me@krake.one>
2018-08-11 20:06:52 +0200

    Minimal reproduction example

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,13 @@
 #![feature(rust_2018_preview)]
 
-#[macro_use]
-extern crate failure;
+macro_rules! m {
+    () => {}
+}
 
-#[macro_use]
-pub mod macros;
-pub mod parser;
-pub mod tokenizer;
-pub mod value;
+mod m {
+    use crate::nonexistent_module::m;
 
-#[cfg(test)]
-mod tests {
-    use super::macros::nix;
-    use super::{
-        parser::{parse as inner_parse, AST},
-        tokenizer::tokenize
-    };
-
-    fn parse(string: &str) -> AST {
-        inner_parse(
-            tokenize(string.chars())
-                .map(|(span, result)| (span, result.expect("error while tokenizing")))
-        ).expect("error while parsing")
-    }
-
-    #[test]
-    fn test_all() {
-        assert_eq!(
-            parse(include_str!("../tests/simple-set.nix")),
-            nix!({
-                int = (3);
-                float = (2.1);
-                string = ("Hello World");
-                multiline = r#"This is a
-multiline
-string :D
-\'\'\'\'\"#;
-            })
-        );
+    fn f() {
+        m!();
     }
 }
