144ad272d22386bca70ed05e05afbc0e555832a7
Jonas Schievink <jonasschievink@gmail.com>
2018-11-13 00:23:06 +0100

    Add a criterion-based benchmark

---
+++ benches/all-packages.rs
@@ -0,0 +1,21 @@
+extern crate rnix;
+#[macro_use]
+extern crate criterion;
+
+use criterion::{Criterion, Benchmark, Throughput};
+use rnix::parse;
+
+fn all_packages(c: &mut Criterion) {
+    let input = include_str!("all-packages.nix");
+    c.bench(
+        "all-packages",
+        Benchmark::new("all-packages", move |b| b.iter(|| {
+                parse(input)
+            }))
+            .throughput(Throughput::Bytes(input.len() as u32))
+            .sample_size(30)
+    );
+}
+
+criterion_group!(benches, all_packages);
+criterion_main!(benches);
