558d86e38f5bae0024ad1af910525bec37398053
jD91mZM2 <me@krake.one>
2019-11-23 17:28:07 +0100

    Use `define_language!` macro of rowan-tools to avoid unsafe

--- src/kinds.rs
+++ src/kinds.rs
@@ -1,105 +1,109 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[repr(u16)]
-#[allow(non_camel_case_types)]
-pub enum SyntaxKind {
-    // Internals
-    TOKEN_COMMENT,
-    TOKEN_ERROR,
-    TOKEN_WHITESPACE,
+use rowan_tools::define_language;
 
-    // Keywords
-    TOKEN_ASSERT,
-    TOKEN_ELSE,
-    TOKEN_IF,
-    TOKEN_IN,
-    TOKEN_INHERIT,
-    TOKEN_LET,
-    TOKEN_REC,
-    TOKEN_THEN,
-    TOKEN_WITH,
+define_language! {
+    language NixLanguage;
 
-    // Symbols
-    TOKEN_CURLY_B_OPEN,
-    TOKEN_CURLY_B_CLOSE,
-    TOKEN_SQUARE_B_OPEN,
-    TOKEN_SQUARE_B_CLOSE,
-    TOKEN_ASSIGN,
-    TOKEN_AT,
-    TOKEN_COLON,
-    TOKEN_COMMA,
-    TOKEN_DOT,
-    TOKEN_ELLIPSIS,
-    TOKEN_QUESTION,
-    TOKEN_SEMICOLON,
+    #[allow(non_camel_case_types)]
+    enum SyntaxKind {
+        // Internals
+        TOKEN_COMMENT,
+        TOKEN_ERROR,
+        TOKEN_WHITESPACE,
 
-    // Operators
-    TOKEN_PAREN_OPEN,
-    TOKEN_PAREN_CLOSE,
-    TOKEN_CONCAT,
-    TOKEN_INVERT,
-    TOKEN_UPDATE,
+        // Keywords
+        TOKEN_ASSERT,
+        TOKEN_ELSE,
+        TOKEN_IF,
+        TOKEN_IN,
+        TOKEN_INHERIT,
+        TOKEN_LET,
+        TOKEN_REC,
+        TOKEN_THEN,
+        TOKEN_WITH,
 
-    TOKEN_ADD,
-    TOKEN_SUB,
-    TOKEN_MUL,
-    TOKEN_DIV,
+        // Symbols
+        TOKEN_CURLY_B_OPEN,
+        TOKEN_CURLY_B_CLOSE,
+        TOKEN_SQUARE_B_OPEN,
+        TOKEN_SQUARE_B_CLOSE,
+        TOKEN_ASSIGN,
+        TOKEN_AT,
+        TOKEN_COLON,
+        TOKEN_COMMA,
+        TOKEN_DOT,
+        TOKEN_ELLIPSIS,
+        TOKEN_QUESTION,
+        TOKEN_SEMICOLON,
 
-    TOKEN_AND,
-    TOKEN_EQUAL,
-    TOKEN_IMPLICATION,
-    TOKEN_LESS,
-    TOKEN_LESS_OR_EQ,
-    TOKEN_MORE,
-    TOKEN_MORE_OR_EQ,
-    TOKEN_NOT_EQUAL,
-    TOKEN_OR,
+        // Operators
+        TOKEN_PAREN_OPEN,
+        TOKEN_PAREN_CLOSE,
+        TOKEN_CONCAT,
+        TOKEN_INVERT,
+        TOKEN_UPDATE,
 
-    // Identifiers and values
-    TOKEN_DYNAMIC_END,
-    TOKEN_DYNAMIC_START,
-    TOKEN_FLOAT,
-    TOKEN_IDENT,
-    TOKEN_INTEGER,
-    TOKEN_INTERPOL_END,
-    TOKEN_INTERPOL_START,
-    TOKEN_PATH,
-    TOKEN_URI,
-    TOKEN_STRING_CONTENT,
-    TOKEN_STRING_END,
-    TOKEN_STRING_START,
+        TOKEN_ADD,
+        TOKEN_SUB,
+        TOKEN_MUL,
+        TOKEN_DIV,
 
-    NODE_APPLY,
-    NODE_ASSERT,
-    NODE_KEY,
-    NODE_DYNAMIC,
-    NODE_ERROR,
-    NODE_IDENT,
-    NODE_IF_ELSE,
-    NODE_SELECT,
-    NODE_INHERIT,
-    NODE_INHERIT_FROM,
-    NODE_STRING,
-    NODE_STRING_INTERPOL,
-    NODE_LAMBDA,
-    // An old let { x = 92; body = x; } syntax
-    NODE_LEGACY_LET,
-    NODE_LET_IN,
-    NODE_LIST,
-    NODE_BIN_OP,
-    NODE_OR_DEFAULT,
-    NODE_PAREN,
-    NODE_PATTERN,
-    NODE_PAT_BIND,
-    NODE_PAT_ENTRY,
-    NODE_ROOT,
-    NODE_ATTR_SET,
-    NODE_KEY_VALUE,
-    NODE_UNARY_OP,
-    NODE_LITERAL,
-    NODE_WITH,
+        TOKEN_AND,
+        TOKEN_EQUAL,
+        TOKEN_IMPLICATION,
+        TOKEN_LESS,
+        TOKEN_LESS_OR_EQ,
+        TOKEN_MORE,
+        TOKEN_MORE_OR_EQ,
+        TOKEN_NOT_EQUAL,
+        TOKEN_OR,
 
-    #[doc(hidden)]
-    __LAST,
+        // Identifiers and values
+        TOKEN_DYNAMIC_END,
+        TOKEN_DYNAMIC_START,
+        TOKEN_FLOAT,
+        TOKEN_IDENT,
+        TOKEN_INTEGER,
+        TOKEN_INTERPOL_END,
+        TOKEN_INTERPOL_START,
+        TOKEN_PATH,
+        TOKEN_URI,
+        TOKEN_STRING_CONTENT,
+        TOKEN_STRING_END,
+        TOKEN_STRING_START,
+
+        NODE_APPLY,
+        NODE_ASSERT,
+        NODE_KEY,
+        NODE_DYNAMIC,
+        NODE_ERROR,
+        NODE_IDENT,
+        NODE_IF_ELSE,
+        NODE_SELECT,
+        NODE_INHERIT,
+        NODE_INHERIT_FROM,
+        NODE_STRING,
+        NODE_STRING_INTERPOL,
+        NODE_LAMBDA,
+        // An old let { x = 92; body = x; } syntax
+        NODE_LEGACY_LET,
+        NODE_LET_IN,
+        NODE_LIST,
+        NODE_BIN_OP,
+        NODE_OR_DEFAULT,
+        NODE_PAREN,
+        NODE_PATTERN,
+        NODE_PAT_BIND,
+        NODE_PAT_ENTRY,
+        NODE_ROOT,
+        NODE_ATTR_SET,
+        NODE_KEY_VALUE,
+        NODE_UNARY_OP,
+        NODE_LITERAL,
+        NODE_WITH,
+
+        #[doc(hidden)]
+        __LAST,
+    }
 }
 use SyntaxKind::*;
 
@@ -124,7 +128,7 @@ impl SyntaxKind {
     pub fn is_fn_arg(self) -> bool {
         match self {
             TOKEN_REC | TOKEN_CURLY_B_OPEN | TOKEN_SQUARE_B_OPEN | TOKEN_PAREN_OPEN
-            | TOKEN_STRING_START | TOKEN_IDENT => true,
+                | TOKEN_STRING_START | TOKEN_IDENT => true,
             _ => self.is_literal(),
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub mod types;
 pub mod value;
 
 pub use self::{
-    kinds::SyntaxKind,
+    kinds::{NixLanguage, SyntaxKind},
     parser::AST,
     value::{StrPart, Value as NixValue},
 };
@@ -18,21 +18,6 @@ pub use rowan::{
 };
 
 use self::tokenizer::Tokenizer;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum NixLanguage {}
-
-impl rowan::Language for NixLanguage {
-    type Kind = SyntaxKind;
-    fn kind_from_raw(raw: rowan::cursor::SyntaxKind) -> Self::Kind {
-        let discriminant: u16 = raw.0;
-        assert!(discriminant <= (SyntaxKind::__LAST as u16));
-        unsafe { std::mem::transmute::<u16, SyntaxKind>(discriminant) }
-    }
-    fn kind_to_raw(kind: Self::Kind) -> rowan::cursor::SyntaxKind {
-        rowan::cursor::SyntaxKind(kind as u16)
-    }
-}
 
 pub type SyntaxNode = rowan::SyntaxNode<NixLanguage>;
 pub type SyntaxToken = rowan::SyntaxToken<NixLanguage>;
