c2faa9b1669c6606bb2dc2e5d852cccc7cf3bc59
Bernardo Meurer <bernardo@meurer.org>
2021-11-14 13:22:06 -0800

    refactor(flake): rework using fenix and buildRustPackage

--- flake.nix
+++ flake.nix
@@ -3,61 +3,76 @@
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    import-cargo.url = github:edolstra/import-cargo;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, import-cargo }:
-    {
-      overlay = final: prev: let
+  outputs = { self, fenix, nixpkgs, utils }: {
+    overlay = final: prev:
+      let
+        fenix' = (fenix.overlay final prev).fenix;
+
+        rustToolchain = fenix'.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-MJyH6FPVI7diJql9d+pifu5aoqejvvXyJ+6WSJDWaIA=";
+        };
+
+        rustPlatform = final.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+
         target = final.rust.toRustTarget final.stdenv.hostPlatform;
-        flags = "--release --offline --target ${target}";
-        inherit (import-cargo.builders) importCargo;
-      in {
-        rnix-parser = final.stdenv.mkDerivation {
+      in
+      {
+        rnix-parser = rustPlatform.buildRustPackage {
           pname = "rnix-parser";
           version = "0.9.1";
+
           src = final.lib.cleanSource ./.;
-          nativeBuildInputs = with final; [
-            rustc cargo
-            (importCargo { lockFile = ./Cargo.lock; inherit (final) pkgs; }).cargoHome
-          ];
+          cargoLock.lockFile = ./Cargo.lock;
+
+          postBuild = "cargo doc";
+
+          doCheck = true;
+          postCheck = "cargo bench";
 
           outputs = [ "out" "doc" ];
-          doCheck = true;
-
-          buildPhase = ''
-            cargo build ${flags}
-            cargo doc ${flags}
-          '';
-
-          checkPhase = ''
-            cargo test ${flags}
-            cargo bench
-          '';
-
           installPhase = ''
+            runHook preInstall
             mkdir -p $out/lib
             mkdir -p $doc/share/doc/rnix
 
-            cp -r ./target/${target}/doc $doc/share/doc/rnix
-            cp ./target/${target}/release/librnix.rlib $out/lib/
+            cp ./target/${target}/$cargoBuildType/librnix.rlib $out/lib
+            cp -r ./target/doc/. $doc/share/doc/rnix
+            runHook postInstall
           '';
+
+          passthru.rustPlatform = rustPlatform;
         };
       };
-    }
-    // utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
-      in
-      rec {
-        # `nix develop`
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ rustfmt rustc cargo ];
-        };
+  }
+  // utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        name = "rnix-parser";
 
-        packages.rnix-parser = pkgs.rnix-parser;
-        defaultPackage = packages.rnix-parser;
-      }
-    );
+        inputsFrom = pkgs.lib.attrValues self.packages.${system};
+
+        nativeBuildInputs = with pkgs; [
+          cargo-edit
+        ];
+      };
+
+      packages.rnix-parser = pkgs.rnix-parser;
+
+      defaultPackage = self.packages.${system}.rnix-parser;
+    }
+  );
 }
