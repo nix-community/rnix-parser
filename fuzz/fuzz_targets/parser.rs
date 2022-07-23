89e81f67c262491fb5493c41fa7df3d0baabf7ca
Brian Shu <littlebubu.shu@gmail.com>
2022-07-23 10:15:48 -0400

    remove printing

--- fuzz/fuzz_targets/parser.rs
+++ fuzz/fuzz_targets/parser.rs
@@ -4,11 +4,7 @@ use libfuzzer_sys::fuzz_target;
 use std::io::{self, Write};
 
 fuzz_target!(|data: &[u8]| {
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-
     if let Ok(text) = std::str::from_utf8(data) {
-        writeln!(handle, "Fuzzing {:?}\n\n", data).unwrap();
         let _ = rnix::Root::parse(text);
     }
 });
