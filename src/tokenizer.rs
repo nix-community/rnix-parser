nix-community/rnix-parser

commit aad261bfa336181c68ac964d1f6a684e050f3314

Fix multiline dollar escape (#102)

src/tokenizer.rs
@@ -124,7 +124,11 @@ impl<'a> Tokenizer<'a> {
                         self.next();
                         self.peek()
                     } {
-                        Some('\'') | Some('\\') | Some('$') => {
+                        Some('\'') | Some('$') => {
+                            self.next().unwrap();
+                        }
+                        Some('\\') => {
+                            self.next().unwrap();
                             self.next().unwrap();
                         }
                         _ => {
@@ -599,6 +603,7 @@ mod tests {
           indented by two
         \'\'\'\'\
         ''${ interpolation was escaped }
+        ''\${ interpolation was escaped }
         two single quotes: '''
         three single quotes: ''''
     '';
@@ -619,6 +624,7 @@ mod tests {
           indented by two
         \'\'\'\'\
         ''${ interpolation was escaped }
+        ''\${ interpolation was escaped }
         two single quotes: '''
         three single quotes: ''''
     "#

commit a5e8bac4c4ba34c1917dc63ea996a7fea5f61aa5

fix error introduced by #102

fuzz/Cargo.toml
@@ -3,7 +3,7 @@ name = "rnix-fuzz"
 version = "0.0.0"
 authors = ["Automatically generated"]
 publish = false
-edition = "2018"
+edition = "2021"
 
 [package.metadata]
 cargo-fuzz = true

src/tokenizer.rs
@@ -108,7 +108,10 @@ impl<'a> Tokenizer<'a> {
         loop {
             let start = self.state;
             match self.next() {
-                None => return TOKEN_ERROR,
+                None => {
+                    self.pop_ctx(Context::StringBody { multiline });
+                    return TOKEN_ERROR;
+                }
                 Some('"') if !multiline => {
                     self.state = start;
                     self.pop_ctx(Context::StringBody { multiline: false });
@@ -131,7 +134,9 @@ impl<'a> Tokenizer<'a> {
                         }
                         Some('\\') => {
                             self.next().unwrap();
-                            self.next().unwrap();
+                            if let None = self.next() {
+                                return TOKEN_ERROR;
+                            }
                         }
                         _ => {
                             self.state = start;
@@ -306,15 +311,15 @@ impl<'a> Tokenizer<'a> {
             }
             '!' => TOKEN_INVERT,
             '{' => {
-                if let Some(Context::Interpol { brackets: extra_brackets }) = self.ctx.last_mut() {
-                    *extra_brackets += 1;
+                if let Some(Context::Interpol { brackets }) = self.ctx.last_mut() {
+                    *brackets += 1;
                 }
                 TOKEN_CURLY_B_OPEN
             }
             '}' => {
-                if let Some(Context::Interpol { brackets: extra_brackets }) = self.ctx.last_mut() {
-                    match extra_brackets.checked_sub(1) {
-                        Some(new) => *extra_brackets = new,
+                if let Some(Context::Interpol { brackets }) = self.ctx.last_mut() {
+                    match brackets.checked_sub(1) {
+                        Some(new) => *brackets = new,
                         None => {
                             self.pop_ctx(Context::Interpol { brackets: 0 });
                             return Some(TOKEN_INTERPOL_END);
@@ -464,6 +469,8 @@ impl<'a> Iterator for Tokenizer<'a> {
 
 #[cfg(test)]
 mod tests {
+    use std::str;
+
     use super::{tokenize, SyntaxKind::*, Token};
 
     macro_rules! tokens {
@@ -480,6 +487,19 @@ mod tests {
     }
     fn error(token: &str) -> Vec<Token<'_>> {
         tokens![(TOKEN_ERROR, token)]
+    }
+
+    fn fuzz<B: AsRef<[u8]>>(b: B) {
+        let s = str::from_utf8(b.as_ref()).unwrap();
+        println!("`{s}`");
+        tokenize(s);
+    }
+
+    #[test]
+    fn test_fuzz() {
+        fuzz("\"");
+        fuzz([39, 39, 34]);
+        fuzz([39, 39, 39, 39, 92]);
     }
 
     #[test]