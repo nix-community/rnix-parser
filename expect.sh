f0752890fb0c81edabc8c98b3af4e3c054fba841
oberblastmeister <61095988+oberblastmeister@users.noreply.github.com>
2022-08-17 10:32:02 -0400

    remove expect.sh (#138)

--- expect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for nix in $(find test_data/ -name "*.nix"); do
-    expected="$(echo -n "$nix" | sed "s/\.nix$//").expect"
-    code="$(cat "$nix")"
-    code="${code%\n}"
-    echo -n "$code" | cargo run --example dump-ast "/dev/stdin" > "$expected" 2>/dev/null
-done
