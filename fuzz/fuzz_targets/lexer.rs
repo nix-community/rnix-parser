ec4e87e8739d6bcef7ae8b4a2b8c93fdaf5e233c
Philip Taron <philip.taron@gmail.com>
2026-01-29 05:00:32 -0800

    chore(fuzz): fix and update fuzz targets
    - Fix lexer target to use public rnix::tokenize() instead of private
      rnix::tokenizer::tokenize()
    - Remove unused imports from parser target
    - Update Cargo.lock with latest compatible dependencies

--- fuzz/fuzz_targets/lexer.rs
+++ fuzz/fuzz_targets/lexer.rs
@@ -3,6 +3,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(text) = std::str::from_utf8(data) {
-        let _ = rnix::tokenizer::tokenize(text);
+        let _ = rnix::tokenize(text);
     }
 });
--- fuzz/fuzz_targets/parser.rs
+++ fuzz/fuzz_targets/parser.rs
@@ -1,8 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-use std::io::{self, Write};
-
 fuzz_target!(|data: &[u8]| {
     if let Ok(text) = std::str::from_utf8(data) {
         let _ = rnix::Root::parse(text);
